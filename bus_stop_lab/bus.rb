5dce6029d337dc68f2074576af3a946ff24075f2
Chris Jennings <chrisjennings555@gmail.com>
2018-10-23 15:29:57 +0100

    upto bus pick up part B

--- bus_stop_lab/bus.rb
+++ bus_stop_lab/bus.rb
@@ -1,14 +1,24 @@
 class Bus
 
-attr_reader :route_number, :destination
+attr_reader :route_number, :destination, :passengers
 
   def initialize (route_number, destination)
     @route_number = route_number
     @destination = destination
+    @passengers = []
   end
 
   def drive
     return "Brum, Brum"
+  end
+
+  # def passengers
+  #   return @passengers
+  # end
+
+  def pick_up (person)
+    @passengers << person
+
   end
 
 end
--- bus_stop_lab/spec/bus_spec.rb
+++ bus_stop_lab/spec/bus_spec.rb
@@ -1,6 +1,7 @@
 require ("minitest/autorun")
 require ("Minitest/rg")
 require_relative ("../bus")
+require_relative ("../person")
 
 class BusTest < MiniTest::Test
 
@@ -28,6 +29,31 @@ class BusTest < MiniTest::Test
   def test_bus_can_drive_?
     expected = "Brum, Brum"
     actual = @bus.drive
+
+    assert_equal(expected, actual)
+
+  end
+
+
+  def test_does_bus_have_passengers
+    expected = []
+    actual = @bus.passengers
+
+    assert_equal(expected, actual)
+  end
+
+  def test_how_many_passengers_on_bus
+    expected = 0
+    actual = @bus.passengers.count
+
+    assert_equal(expected, actual)
+  end
+
+  def test_additon_of_passenger
+    @bus.pick_up(Person.new("Chris", 30))
+
+    expected = 1
+    actual = @bus.passengers.count
 
     assert_equal(expected, actual)
 
